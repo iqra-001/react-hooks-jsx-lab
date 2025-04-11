0af584e36f50216d5aeba16fc277ba28d9fd8e25
iqra <abdikarimiqra@gmail.com>
2025-04-11 14:14:11 +0300

    index.html

--- src/data/data.js
+++ src/data/data.js
@@ -3,5 +3,7 @@ const name = "Liza";
 const city = "New York";
 
 const image = "https://i.imgur.com/mV8PQxj.gif";
+// const h1 = screen.queryByText(`${name} is a Web Developer from ${city}`);
+// expect(h1).toHaveStyle({ color: "firebrick" });
 
 export { name, city, image };
